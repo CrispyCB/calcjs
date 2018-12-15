3f6b73388ace713804142217d0fdc164f64ad80c
Christian Bahnweg <cebahnweg@gmail.com>
2018-12-15 13:52:59 -0500

    declares global let variable and constant operation functions

---
+++ main.js
@@ -0,0 +1,7 @@
+//declare global let variable and constant operation functions.
+let operation;
+
+const multiply = (a,b) => {return a * b;}
+const divide = (a,b) => {return a / b;}
+const add = (a,b) => {return a + b;}
+const subtract = (a,b) => {return a - b;}
